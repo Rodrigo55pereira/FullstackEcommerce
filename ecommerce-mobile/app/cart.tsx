ba8f57fa7a051f2509f9e43fa979791b78e342b1
Rodrigo55pereira <rodrigo55pereira.com>
2025-01-17 11:24:48 -0300

    setup expo router

---
+++ ecommerce-mobile/app/cart.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+export default function CartScreen(){
+    return(
+        <View>
+            <Text style={{
+                fontSize: 30
+            }}>
+                Cart Screen
+            </Text>
+        </View>
+    )
+}
---
+++ ecommerce-mobile/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+export default function HomeScreen(){
+    return(
+        <View>
+            <Text style={{
+                fontSize: 30
+            }}>
+                Home Screen
+            </Text>
+        </View>
+    )
+}
