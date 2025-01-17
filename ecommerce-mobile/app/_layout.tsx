a7852eaf0d2fca8feb1b314e0d2c9b74cc73669c
Rodrigo55pereira <rodrigo55pereira.com>
2025-01-17 14:50:43 -0300

    install gluestack

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -1,7 +1,12 @@
-import { Slot, Stack, Tabs } from "expo-router"
+import "@/global.css";
 
-export default function RootLayout(){
-    return(
-        <Stack />
+import { Slot, Stack, Tabs } from "expo-router"
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider"
+
+export default function RootLayout() {
+    return (
+        <GluestackUIProvider>
+            <Stack />
+        </GluestackUIProvider>
     )
 }
--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,9 +1,18 @@
 import { View, Text, FlatList } from 'react-native'
 import products from '../assets/products.json'
 import ProductListItem from '../components/ProductListItem'
+import { Button, ButtonText } from '@/components/ui/button'
 
 
 export default function HomeScreen() {
+    
+    return(
+        <Button variant='outline'>
+            <ButtonText>Press me</ButtonText>
+        </Button>
+    )
+    
+    /*
     return (
 
         <FlatList
@@ -13,4 +22,5 @@ export default function HomeScreen() {
             }
         />
     )
+    */
 }
