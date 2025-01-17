28fc7ef3639c3307ef3e5ba1d241f8a518e385e4
Rodrigo55pereira <rodrigo55pereira.com>
2025-01-17 14:07:07 -0300

    Products list structure

---
+++ ecommerce-mobile/components/ProductListItem.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native'
+
+export default function ProductListItem({product}){
+    return(
+        <Text style={{
+            fontSize: 30
+        }}>
+            {product.name}
+        </Text>
+    )
+}
